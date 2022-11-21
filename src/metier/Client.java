2b9e3233c6d708ef115f83263cff15fd42d81480
Mariecls <marie.ucles@neuf.fr>
2022-11-21 12:02:14 +0100

    Client

--- src/metier/Client.java
+++ src/metier/Client.java
@@ -51,7 +51,15 @@ public class Client
 	}
 	
 
+	void add(Facture commande)
+	{
+		factures.add(commande);
+	}
 
+	void remove(Facture commande)
+	{
+		factures.remove(commande);
+	}
 	/**
 	 * Retourne une copie de la liste des factures du client. 
 	 * @return une copie de la liste des factures du client.
